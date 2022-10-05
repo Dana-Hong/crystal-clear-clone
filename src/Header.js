216d9adcb4907ba4c0e85133ccd9558064b7ed45
Dana <dana.hong@hotmail.com>
2022-10-05 19:18:59 -0400

    Add toggle Nav functionality to hamburger button

--- src/Header.js
+++ src/Header.js
@@ -11,15 +11,20 @@ export default function Header(props) {
                     <img className="logo" src={logo} alt="logo"></img>
                 </Link>
             </div>
+            <button className="nav-toggle" aria-label="toggle navigation" onClick={props.toggleNav}>
+                <span className="hamburger"></span>
+            </button>
             <nav className="nav">
-                <Link to='/'><span className="nav__item">Home</span></Link>
-                <Link to='/products'><span className="nav__item">Products</span></Link>
-                <Link to='/service'><span className="nav__item">Service</span></Link>
-                <Link to='/flyer'><span className="nav__item">Flyer</span></Link>
-                <Link to='/financing'><span className="nav__item">Financing</span></Link>
-                <Link to='/about'><span className="nav__item">About us</span></Link>
-                <Link to='/contact'><span className="nav__item">Contact</span></Link>
+                <div className="nav__list">
+                    <Link to='/'><span className="nav__item">Home</span></Link>
+                    <Link to='/products'><span className="nav__item">Products</span></Link>
+                    <Link to='/service'><span className="nav__item">Service</span></Link>
+                    <Link to='/flyer'><span className="nav__item">Flyer</span></Link>
+                    <Link to='/financing'><span className="nav__item">Financing</span></Link>
+                    <Link to='/about'><span className="nav__item">About us</span></Link>
+                    <Link to='/contact'><span className="nav__item">Contact</span></Link>
+                </div>
             </nav>
         </header>
-    )
+    );
 }
